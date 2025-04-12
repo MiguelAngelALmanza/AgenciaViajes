7154d62e67b2bb8e45f8923cddfd8c0983746e65
Miguel Almanza <202300135@est.umss.edu>
2025-04-11 21:51:44 -0400

    Ventanas Interfaz y registro (falta decorar)V1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
-import org.example.Vista.RegistroVista;
-import org.example.Controlador.UsuarioControlador;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.example.Vista.LoginVista;
+import org.example.Controlador.LoginControlador;
+
 public class Main {
     public static void main(String[] args) {
-        RegistroVista vista = new RegistroVista();
-        new UsuarioControlador(vista);
-        vista.setVisible(true);
+        LoginVista vistaLogin = new LoginVista();
+        new LoginControlador(vistaLogin);
+        vistaLogin.setVisible(true);
     }
 }
