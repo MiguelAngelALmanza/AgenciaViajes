ff496049becd2dac80ab55cfc8a79692878765c4
carlos <carlosarturoteranclaros@gmail.com>
2025-04-15 20:12:38 -0400

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/example/Vista/CatalogoVista.java
+++ src/main/java/org/example/Vista/CatalogoVista.java
@@ -11,7 +11,7 @@ public class CatalogoVista extends JFrame {
     private JButton botonRecargar;
     private JButton botonVerDetalles;
     private JButton botonVerReseñas; // NUEVO BOTÓN
-
+    private JButton botonColaboradores;
     public CatalogoVista() {
         setTitle("Catálogo de Destinos");
         setSize(500, 400);
@@ -26,11 +26,13 @@ public class CatalogoVista extends JFrame {
         botonRecargar = new JButton("Recargar Catálogo");
         botonVerDetalles = new JButton("Ver Detalles");
         botonVerReseñas = new JButton("Ver Reseñas"); // Instanciar nuevo botón
+        botonColaboradores = new JButton("Conoce a nuestros colaboradores"); // NUEVO
 
         JPanel panelBotones = new JPanel();
         panelBotones.add(botonRecargar);
         panelBotones.add(botonVerDetalles);
         panelBotones.add(botonVerReseñas); // Agregarlo al panel
+        panelBotones.add(botonColaboradores); // NUEVO
 
         add(scroll, BorderLayout.CENTER);
         add(panelBotones, BorderLayout.SOUTH);
@@ -44,6 +46,10 @@ public class CatalogoVista extends JFrame {
         botonRecargar.addActionListener(e -> controlador.cargarDestinos());
         botonVerDetalles.addActionListener(e -> controlador.verDetallesSeleccionado());
         botonVerReseñas.addActionListener(e -> controlador.verReseñasSeleccionadas()); // Nueva acción
+        botonColaboradores.addActionListener(e -> {
+            new org.example.Guiaturistico.Guiaturistico().setVisible(true);
+        });
+
     }
 
     public JTextArea getAreaDestinos() {
